69d3063b950faafcd1921b11d8370f2d448c5125
Justin Marciszewski <justinjm@google.com>
2023-05-23 14:38:38 -0400

    generate test json

---
+++ tests/ex-import.py
@@ -0,0 +1,17 @@
+import json
+
+with open('example_requests.json', 'r') as f:
+    obj = json.load(f)
+
+# print(obj)
+
+# request_json = obj.get_json(silent=True)
+# calls = request_json['calls']
+
+calls = obj['calls']
+print(calls)
+
+for call in calls:
+    # set tableId as variable for passing into rowAccessPolicies API call
+    projectId = call[0]
+    print(projectId)
---
+++ tests/generate_json.py
@@ -0,0 +1,26 @@
+import json
+import requests
+
+obj = {
+    "requestId": "124ab1c",
+    "caller": "//bigquery.googleapis.com/projects/myproject/jobs/myproject:US.bquxjob_5b4c112c_17961fafeaf",
+    "sessionUser": "test-user@test-company.com",
+    "userDefinedContext": {
+        "key1": "value1",
+        "key2": "v2"
+    },
+    "calls": [
+        ["demos-vertex-ai", "z_test", "crm_account"],
+        ["demos-vertex-ai", "z_test", "crm_account"]
+    ]
+}
+
+for _ in range(10):
+    obj["calls"].append(["demos-vertex-ai", "z_test", "crm_account"])
+
+print(obj)
+
+# Write the modified object to a new file
+with open('example_requests.json', 'w') as f:
+    json.dump(obj, f)
+
