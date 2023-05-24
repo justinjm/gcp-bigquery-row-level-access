5a733dafe31aff5430c30e9601c49c487a224cf9
Justin Marciszewski <justinjm@google.com>
2023-05-24 16:21:02 -0400

    initial commit - hello world aysncio

---
+++ tests/local_async_simple.py
@@ -0,0 +1,11 @@
+import asyncio
+import aiohttp 
+
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        async with session.get('http://httpbin.org/get') as resp:
+            print(resp.status)
+            print(await resp.text())
+
+asyncio.run(main())
