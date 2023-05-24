e72811c9e488f1b8b5dcd36aea964f5d85b7f54b
Justin Marciszewski <justinjm@google.com>
2023-05-24 16:31:54 -0400

    simple test successful

--- tests/local_async_simple.py
+++ tests/local_async_simple.py
@@ -1,11 +1,28 @@
 import asyncio
 import aiohttp 
+import json
+from google.auth import default
+from google.auth.transport.requests import Request
 
+projectId = "demos-vertex-ai"
+datasetId = "z_test"
+tableId = "crm_account"
+url = f"https://bigquery.googleapis.com/bigquery/v2/projects/{projectId}/datasets/{datasetId}/tables/{tableId}/rowAccessPolicies"
 
 async def main():
+    # Use the default credentials to obtain an access token
+    creds, _ = default(scopes=["https://www.googleapis.com/auth/bigquery"])
+    creds.refresh(Request())
+
+    # Set the authorization header using the access token
+    headers = {
+        "Authorization": f"Bearer {creds.token}",
+        "Content-Type": "application/json"
+    }
+
     async with aiohttp.ClientSession() as session:
-        async with session.get('http://httpbin.org/get') as resp:
+        async with session.get(url, headers=headers) as resp:
             print(resp.status)
-            print(await resp.text())
+            print(await resp.json())
 
 asyncio.run(main())
