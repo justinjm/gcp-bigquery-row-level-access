6c578e8459eb8869c590227a67cc1a12b7ca006e
Justin Marciszewski <justinjm@google.com>
2023-05-25 09:07:58 -0400

    cleanup

--- tests/02_generate_sample_request_json.py
+++ tests/02_generate_sample_request_json.py
@@ -1,5 +1,8 @@
 import json
 
+# number of calls to generate 
+n = 8 
+
 obj = {
     "requestId": "124ab1c",
     "caller": "//bigquery.googleapis.com/projects/myproject/jobs/myproject:US.bquxjob_5b4c112c_17961fafeaf",
@@ -9,15 +12,15 @@ obj = {
         "key2": "v2"
     },
     "calls": [
-        ["demos-vertex-ai", "z_test", "crm_account"],
         ["demos-vertex-ai", "z_test", "crm_account"]
     ]
 }
 
-for _ in range(10):
+## generate calls objects, minus 1  since we want a round number and have 
+## one set above for example 
+for _ in range(n-1):
     obj["calls"].append(["demos-vertex-ai", "z_test", "crm_account"])
 
 # Write the modified object to a new file
 with open('example_requests.json', 'w') as f:
     json.dump(obj, f)
-
--- tests/03_import_sample_request_json.py
+++ tests/03_import_sample_request_json.py
@@ -4,14 +4,3 @@ with open('example_requests.json', 'r') as f:
     obj = json.load(f)
 
 print(obj)
-
-# request_json = obj.get_json(silent=True)
-# calls = request_json['calls']
-
-# calls = obj['calls']
-# print(calls)
-
-# for call in calls:
-#     # set tableId as variable for passing into rowAccessPolicies API call
-#     projectId = call[0]
-#     print(projectId)
