ee994141d348d642bd1042e241172a6c1791f982
Justin Marciszewski <justinjm@google.com>
2023-05-25 13:05:12 -0400

    successful hello world test

--- tests/asyncio_helloworld.py
+++ tests/asyncio_helloworld.py
@@ -1,18 +1,28 @@
 import asyncio
 from aiohttp import ClientSession
+import json
 
-async def hello(url: str):
+async def hello(url: str, queue: asyncio.Queue):
     async with ClientSession() as session:
         async with session.get(url) as response:
-            response = await response.text()
-            print(response)
+            result = {"response": await response.text(), "url": url}
+            await queue.put(result)
+
 
 async def main():
-    tasks = []
     # I'm using test server localhost, but you can use any url
-    url = "http://httpbin.org/get"
+    ## start via: `$ cd {local_directory} && python3 -m http.server`
+    url = "http://localhost:8000/data-misc"
+    results = []
+    queue = asyncio.Queue()
     async with asyncio.TaskGroup() as group:
         for i in range(10):
-            group.create_task(hello(url.format(i)))
+            group.create_task(hello(url.format(i), queue))
+
+    while not queue.empty():
+        results.append(await queue.get())
+    
+    print(json.dumps(results))
+
 
 asyncio.run(main()) 
